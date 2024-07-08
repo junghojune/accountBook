8c7fe1815859c1689db011097365b8051cb21c2f
hosu <hojune0904@gmail.com>
2024-07-08 18:09:14 +0900

    공통 response 적용

---
+++ src/main/java/com/hosu/accountbook/controller/AccountBookController.java
@@ -0,0 +1,1 @@
+package com.hosu.accountbook.controller;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;@RestController@RequestMapping("/api/v1/transactions")public class AccountBookController {}
